fb48fe7a6f7b178531f11cbc3039bf07803cc6f4
wyh <18720913113@163.com>
2017-10-27 17:00:02 +0800

    增加ion-input及input双向数据绑定

--- src/directives/numpad.ts
+++ src/directives/numpad.ts
@@ -1,44 +1,75 @@
-import { Directive, Renderer2, ElementRef, ViewContainerRef, ComponentFactoryResolver, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, Renderer2, ViewContainerRef, ComponentFactoryResolver, HostListener, Input, Output, EventEmitter, forwardRef } from '@angular/core';
 import { MdpNumpadComponent } from '../components/mdp-numpad';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Content } from 'ionic-angular';
 
 /**
  * Generated class for the NumpadDirective directive.自定义数字键盘
  */
+
+const CURRENCY_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => NumpadDirective),
+  multi: true
+}
+
 @Directive({
-  selector: '[numpad]', // Attribute selector,
+  selector: '[numpad]',
+  host: {
+    '(change)': 'handleInput($event.target.value)',
+  },
+  providers: [CURRENCY_VALUE_ACCESSOR]
 })
-export class NumpadDirective {
+export class NumpadDirective implements ControlValueAccessor {
+  onChange = (_: any) => { };
+  onTouched = () => { };
   isShow: boolean = false;
   @Input() isInit: any = false;
   @Input() position: ViewContainerRef;
   @Input() content: Content;
+  @Input('padModel') any;
+  @Output('padModelChange') padModelChange: EventEmitter<number> = new EventEmitter<number>();
+  inputElement: HTMLInputElement = null;
   _vHeight: number = document.body.clientHeight;
   _topHeight: number = this._vHeight * 0.64;
   @HostListener("click", ["$event"])
   onClick(e) {
     this.el.nativeElement.localName === "ion-input" ? this.el.nativeElement.children[0].blur() :
       this.el.nativeElement.blur();
-
-    //若当前输入框位置低于键盘  则向上滚动至键盘上方
     if (e.clientY + this.el.nativeElement.clientHeight > this._topHeight) {
       this.content.scrollTo(0, this.content.getContentDimensions().scrollTop + e.clientY + this.el.nativeElement.clientHeight - this._topHeight, 300);
     }
-
     !this.isShow && this.showNumPad();
   }
 
-  constructor(private render: Renderer2, private numPadel: ElementRef, private el: ElementRef, private view: ViewContainerRef, private cfr: ComponentFactoryResolver) {
-    console.log(el)
-  }
+  constructor(private render: Renderer2, private numPadel: ElementRef, private el: ElementRef, private view: ViewContainerRef, private cfr: ComponentFactoryResolver) {}
 
-  ngOnInit() {
-    // setTimeout(()=>{
-    //   this.el.nativeElement.focus();
-    // },100)
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    let element = this.el.nativeElement;
+
+    if (element.tagName === 'INPUT') {
+      this.inputElement = element;
+    }
+    else {
+      this.inputElement = element.getElementsByTagName('input')[0];
+    }
+
     if (this.isInit === true || this.isInit === "true" || this.isInit === "") {
       this.showNumPad()
     }
+
+  }
+
+  registerOnChange(fn: (_: any) => void): void { this.onChange = fn; };
+
+  registerOnTouched(fn: () => void): void { this.onTouched = fn; };
+
+  writeValue(value: any): void { };
+
+  handleInput(value: any) {
+    this.padModelChange.next(value);
   }
 
   /**
@@ -66,13 +97,11 @@ export class NumpadDirective {
         }, 300);
         this.isShow = false;
       } else {
-        this.render.setAttribute(this.el.nativeElement, "value", finalDv.id);
+        this.onChange(finalDv.id);
+        this.handleInput(finalDv.id);
+        this.render.setProperty(this.inputElement, 'value', finalDv.id);
       }
     })
-  }
-
-  ngAfterViewInit() {
-
   }
 
 }
